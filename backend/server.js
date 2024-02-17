971e18a749241a5154837cf68c9b7d8dbaa4b5bf
Lars Dev <janlarsaustero@gmail.com>
2024-02-17 12:05:17 -0500

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 require('dotenv').config()
 const workoutRoutes = require('./routes/workouts')
+const mongoose = require('mongoose')
+const uri = process.env.MONG_URI
+
 
 const app = express()
 
@@ -13,6 +16,14 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts', workoutRoutes)
 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000')
-})
+mongoose.connect(uri)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db and listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
