d7fa69ee556b0f9ece400c5c025b2af4a1c94f5c
akosilar <55524614+akosilar@users.noreply.github.com>
2024-02-17 15:58:27 -0500

    added validation for finding a valid ID

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,5 +1,5 @@
 const Workout = require('../models/workoutModel')
-
+const mongoose = require('mongoose')
 //get all workouts
 const getWorkouts = async (req, res) => {
     const workouts = await Workout.find({}).sort({ createdAt: -1 })
@@ -11,6 +11,10 @@ const getWorkouts = async (req, res) => {
 // get single workout
 const getWorkout = async (req, res) => {
     const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: "no such workout" })
+    }
     const workout = await Workout.findById(id)
 
     if (!workout) {
